af0afbb72aaaacd04a738dc403372e1c18d4c752
Piotr <piotr@piotrf.pl>
2018-01-23 19:14:05 +0100

    Abstract catalog theme.

--- packages/styleguide/catalog/index.js
+++ packages/styleguide/catalog/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Catalog, pageLoader } from "catalog";
 
 import InterviewJSLogo from "./static/interviewjs-logo.png";
+import theme from "./theme";
 
 import { Styles, Reset } from "../utils";
 
@@ -31,22 +32,10 @@ const pages = [
 
 ReactDOM.render(
   <Catalog
-    styles={[Styles]}
     logoSrc={InterviewJSLogo}
-    theme={{
-      background: "#f7f7f7",
-      linkColor: "#495abd",
-      textColor: "#000143",
-
-      lightColor: "#f7f7f7",
-
-      pageHeadingBackground: "#333",
-
-      brandColor: "#495abd",
-
-      navBarTextColor: "#000143"
-    }}
     pages={pages}
+    styles={[Styles]}
+    theme={theme}
     title="InterviewJS UI Library"
   />,
   document.getElementById("catalog")
