2de87cc21ec66f27e3415b8355c1fd4e696e853f
Piotr F <hello@piotrf.pl>
2018-02-09 13:39:20 +0100

    Display first data in the Composer.

--- packages/composer/src/views/Composer.js
+++ packages/composer/src/views/Composer.js
@@ -3,8 +3,11 @@ import React from "react";
 import { Container } from "interviewjs-styleguide";
 
 const Composer = props => {
+  const { storyId } = props.params;
+  const i = props.stories.findIndex(story => story.id === storyId);
+  const story = props.stories[i];
   console.log(props);
-  return <Container>Composer</Container>;
+  return <Container>{story.title}</Container>;
 };
 
 export default Composer;
