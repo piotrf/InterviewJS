be976005f2681fe09167b09f675c225bfb319444
Piotr F <hello@piotrf.pl>
2018-02-12 03:32:16 +0100

    Populate some more stories.

--- packages/composer/src/data/stories.js
+++ packages/composer/src/data/stories.js
@@ -41,6 +41,41 @@ const stories = [
     byline: {
       author: "Laurian g.",
       url: "https://twitter.com/gridinoc",
+      moddate: "01-12-2016"
+    },
+    intro: "Short Intro",
+    context: "Context",
+    media: {
+      cover: "",
+      logo: "",
+      attachments: [{ url: "", id: "" }]
+    },
+    interviewees: [
+      {
+        name: "Techcrunch",
+        bio: "",
+        title: "",
+        avatar:
+          "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg",
+        color: "green"
+      },
+      {
+        name: "The Guardian",
+        bio: "",
+        title: "",
+        avatar:
+          "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg",
+        color: "blue"
+      }
+    ],
+    storyline: []
+  },
+  {
+    title: "Another interviewJS story",
+    id: "11610229664069df501",
+    byline: {
+      author: "Laurian g.",
+      url: "https://twitter.com/gridinoc",
       moddate: "24-07-2016"
     },
     intro: "Short Intro",
@@ -51,6 +86,58 @@ const stories = [
       attachments: [{ url: "", id: "" }]
     },
     interviewees: [
+      {
+        name: "Techcrunch",
+        bio: "",
+        title: "",
+        avatar:
+          "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg",
+        color: "green"
+      },
+      {
+        name: "The Guardian",
+        bio: "",
+        title: "",
+        avatar:
+          "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg",
+        color: "blue"
+      }
+    ],
+    storyline: []
+  },
+  {
+    title: "Yet another interview",
+    id: "1161022d66406956501",
+    byline: {
+      author: "Laurian g.",
+      url: "https://twitter.com/gridinoc",
+      moddate: "12-01-2015"
+    },
+    intro:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    context: "Context",
+    media: {
+      cover: "",
+      logo: "",
+      attachments: [{ url: "", id: "" }]
+    },
+    interviewees: [
+      {
+        name: "Ali R.",
+        bio: "Some bio",
+        title: "A title",
+        avatar:
+          "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg",
+        color: "yellow"
+      },
+      {
+        name: "Piotr F.",
+        bio: "Some bio",
+        title: "A title",
+        avatar:
+          "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg",
+        color: "purple"
+      },
       {
         name: "Techcrunch",
         bio: "",
