9f413fa50e3190ab31c90b13c261681f744f9977
Piotr F <hello@piotrf.pl>
2018-02-19 17:41:03 +0100

    Give our stories dataset some love.

--- packages/composer/src/data/stories.js
+++ packages/composer/src/data/stories.js
@@ -1,4 +1,166 @@
+import uuidv4 from "uuid/v4";
+
 const stories = [
+  {
+    title: "Meeting Ava",
+    id: "0bf34b30-743b-46ab-weewe-c875326d86f6",
+    author: "Alex Garland",
+    authorLink: "http://www.imdb.com/name/nm0307497/",
+    pubDate: "00-00-2014",
+    modDate: 1517999999478,
+    intro:
+      "Meet and interact with Ava. In the end, you’ll be asked to decide wether you’re chatting to a real person or a computer bot.",
+    context:
+      "The Turing test, developed by Alan Turing in 1950, is a test of a machine's ability to exhibit intelligent behavior equivalent to, or indistinguishable from, that of a human. Turing proposed that a human evaluator would judge natural language conversations between a human and a machine designed to generate human-like responses. The evaluator would be aware that one of the two partners in conversation is a machine, and all participants would be separated from one another. The conversation would be limited to a text-only channel such as a computer keyboard and screen so the result would not depend on the machine's ability to render words as speech.[2] If the evaluator cannot reliably tell the machine from the human, the machine is said to have passed the test. The test does not check the ability to give correct answers to questions, only how closely answers resemble those a human would give.",
+    media: {
+      cover: "",
+      logo: "",
+      attachments: [{ url: "", id: "" }]
+    },
+    interviewees: [
+      {
+        name: "Ava",
+        bio:
+          "Ava has a robotic body but a human-looking face, and is confined to your browser window.",
+        title: "Humanoid robot with artificial intelligence",
+        avatar:
+          "http://3.bp.blogspot.com/-4LhUwagGzfA/VUfQAIEtV3I/AAAAAAAAFZQ/dTm0p-y9OqM/s1600/EX-MACHINA%2B1.jpg",
+        color: "#88a3a0",
+        storyline: [
+          {
+            id: uuidv4(),
+            content: "Hello.",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "…Hi.",
+            role: "user"
+          },
+          {
+            id: uuidv4(),
+            content: "Who are you?",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "I'm Caleb.",
+            role: "user"
+          },
+          {
+            id: uuidv4(),
+            content: "Hello, Caleb.",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "Hello, Caleb.",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "…Do you have a name?",
+            role: "user"
+          },
+          {
+            id: uuidv4(),
+            content: "Yes.",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "Ava.",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "…I'm pleased to meet you, Ava.",
+            role: "user"
+          },
+          {
+            id: uuidv4(),
+            content: "I'm pleased to meet you too.",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "Are you nervous?",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "…Why do you ask that?",
+            role: "user"
+          },
+          {
+            id: uuidv4(),
+            content: "Are you nervous?",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "…Yes. A little.",
+            role: "user"
+          },
+          {
+            id: uuidv4(),
+            content: "Why?",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "I'm not sure.",
+            role: "user"
+          },
+          {
+            id: uuidv4(),
+            content: "I feel nervous too.",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "…Do you?",
+            role: "user"
+          },
+          {
+            id: uuidv4(),
+            content: "Yes.",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "Why do you feel nervous?",
+            role: "user"
+          },
+          {
+            id: uuidv4(),
+            content: "I've never met anyone new before. Only Nathan.",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "…Then we're both in quite a similar position.",
+            role: "user"
+          },
+          {
+            id: uuidv4(),
+            content: "Haven't you met lots of new people before?",
+            role: "interviewee"
+          },
+          {
+            id: uuidv4(),
+            content: "None like you.",
+            role: "user"
+          },
+          {
+            id: uuidv4(),
+            content: "Oh.",
+            role: "interviewee"
+          }
+        ]
+      }
+    ]
+  },
   {
     title: "Obamacare — One Year In",
     id: "0bf34b30-743b-46ab-9788-c875326d86f6",
