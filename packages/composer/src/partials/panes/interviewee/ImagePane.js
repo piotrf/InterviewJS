d5e5de4dfcb8698d31f90eebd00e8963c1a113d6
Piotr F <hello@piotrf.pl>
2018-03-12 14:52:23 +0100

    Add Image bubble handling.

--- packages/composer/src/partials/panes/interviewee/ImagePane.js
+++ packages/composer/src/partials/panes/interviewee/ImagePane.js
@@ -1,6 +1,14 @@
-import { shape, string } from "prop-types";
+import { func, shape, string } from "prop-types";
 import React, { Component } from "react";
 
+import {
+  BubbleHTMLWrapper,
+  Form,
+  FormItem,
+  Label,
+  Legend,
+  TextInput
+} from "interviewjs-styleguide";
 import PaneFrame from "../PaneFrame";
 
 export default class ImagePane extends Component {
@@ -9,11 +17,44 @@ export default class ImagePane extends Component {
     this.state = {
       draft: this.props.draft
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ draft: { ...this.state.draft, [name]: value } }, () =>
+      this.props.updateDraft(this.state.draft)
+    );
   }
   render() {
     return (
-      <PaneFrame {...this.props} draft={null} side="left">
-        ImagePane
+      <PaneFrame
+        {...this.props}
+        draft={
+          <div>
+            <BubbleHTMLWrapper>
+              <img src={this.state.draft.value} alt="" />
+            </BubbleHTMLWrapper>
+          </div>
+        }
+        hasDraft={this.props.draft.value !== ""}
+        side="left"
+      >
+        <Form>
+          <FormItem>
+            <Label>Upload image</Label>
+            <TextInput
+              text
+              name="value"
+              onChange={(e) => this.handleChange(e)}
+              required
+              type="file"
+              accept="image/gif, image/jpeg, image/jpg, image/svg, image/gif, image/png"
+            />
+            <Legend tip="Select an image with extension of .png, .jpg, .jpeg, .svg or .gif">
+              i
+            </Legend>
+          </FormItem>
+        </Form>
       </PaneFrame>
     );
   }
@@ -23,7 +64,8 @@ ImagePane.propTypes = {
   draft: shape({
     value: string,
     title: string
-  })
+  }),
+  updateDraft: func.isRequired
 };
 
 ImagePane.defaultProps = {
