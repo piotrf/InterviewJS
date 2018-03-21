e322f91a663b096ae3a2a211d851b70f19f1da6b
Laurian Gridinoc <laurian@gridinoc.name>
2018-03-21 20:08:15 +0700

    version data

--- packages/composer/config-overrides.js
+++ packages/composer/config-overrides.js
@@ -7,11 +7,16 @@ const path = require("path");
 const fs = require("fs");
 
 const rewireBabelLoader = require("react-app-rewire-babel-loader");
+const rewireDefinePlugin = require("react-app-rewire-define-plugin");
+const GitRevisionPlugin = require("git-revision-webpack-plugin");
 
 // helpers
 
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const gitRevisionPlugin = new GitRevisionPlugin({
+  versionCommand: 'describe --always --tags --dirty'
+});
 
 module.exports = function override(config, env) {
   // white-list some npm modules to the babel-loader pipeline
@@ -26,6 +31,11 @@ module.exports = function override(config, env) {
   // see: https://webpack.js.org/configuration/module/#rule-exclude
 
   config = rewireBabelLoader.exclude(config, /(node_modules|bower_components)/);
+
+  // Use `webpack.DefinePlugin` to add the version number from package.json
+  config = rewireDefinePlugin(config, env, {
+    "process.env.VERSION": JSON.stringify(gitRevisionPlugin.version())
+  });
 
   return config;
 };
