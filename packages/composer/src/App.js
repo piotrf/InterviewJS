f8fa3463a3645b8d30818215dd2d1300e760a5f4
Laurian Gridinoc <laurian@gridinoc.name>
2018-03-22 05:31:50 +0700

    clean-up

--- packages/composer/src/App.js
+++ packages/composer/src/App.js
@@ -1,7 +1,6 @@
 import { cloneElement } from "react";
 import { compose, bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { firebaseConnect, isLoaded, isEmpty } from "react-redux-firebase";
 import * as actionCreators from "./actions/actionCreators";
 
 const mapStateToProps = (state) => ({
@@ -15,14 +14,3 @@ const mapDispatchToProps = (dispatch) =>
 const Main = (props) => cloneElement(props.children, props);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
-
-// export default compose(
-//   firebaseConnect([
-//     'stories' // { path: '/stories' } // object notation
-//   ]),
-//   connect((state) => ({
-//     stories: state.firebase.data.stories,
-//     user: state.user,
-//     // profile: state.firebase.profile // load profile
-//   }), mapDispatchToProps)
-// )(Main)
