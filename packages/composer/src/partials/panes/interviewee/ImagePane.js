00f430bad9fc219fc1732e7d6947d3de2b372f0a
Laurian Gridinoc <laurian@gridinoc.name>
2018-03-23 10:15:41 +0700

    image resize in bubbles

--- packages/composer/src/partials/panes/interviewee/ImagePane.js
+++ packages/composer/src/partials/panes/interviewee/ImagePane.js
@@ -1,6 +1,7 @@
 import { func, shape, string } from "prop-types";
 import React, { Component } from "react";
 import Dropzone from "react-dropzone";
+import Pica from "pica";
 
 import {
   BubbleHTMLWrapper,
@@ -18,7 +19,9 @@ export default class ImagePane extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      draft: this.props.draft
+      draft: this.props.draft,
+      type: "image/*",
+      size: 0
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -31,23 +34,46 @@ export default class ImagePane extends Component {
     return null;
   }
 
+  onLoad() {
+    const targetWidth = this.img.width > 600 ? 600 : this.img.width;
+    const targetHeight = parseInt(targetWidth * this.img.height / this.img.width);
+    console.log(`${this.img.width} x ${this.img.height} => ${targetWidth} x ${targetHeight}`);
+    const offScreenCanvas = document.createElement('canvas');
+    offScreenCanvas.width  = targetWidth;
+    offScreenCanvas.height = targetHeight;
+
+    const pica = Pica({ features: ['js', 'wasm', 'ww'] });
+    pica.resize(this.img, offScreenCanvas, {
+      unsharpAmount: 80,
+      unsharpRadius: 0.6,
+      unsharpThreshold: 2,
+      transferable: true
+    }).then(result => pica.toBlob(result, 'image/jpeg', 0.90))
+      .then(blob => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          const base64data = reader.result.length > 3e6 ? '' : reader.result;
+          console.log("data url length", base64data.length);
+          this.setState({ draft: { ...this.state.draft, value: base64data } }, () =>
+            this.props.updateDraft(this.state.draft)
+          );
+        };
+        reader.readAsDataURL(blob);
+      })
+      .catch(error => console.log(error));
+  }
+
+  handleFile(f) {
+    const { type, size, preview } = f[0];
+    this.img.src = preview;
+    this.setState({ type, size });
+  }
+
   handleChange(e) {
     const { name, value } = e.target;
     this.setState({ draft: { ...this.state.draft, [name]: value } }, () =>
       this.props.updateDraft(this.state.draft)
     );
-  }
-
-  handleFile(f) {
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      const base64data = reader.result.length > 3e6 ? '' : reader.result;
-      console.log(base64data);
-      this.setState({ draft: { ...this.state.draft, value: base64data } }, () =>
-        this.props.updateDraft(this.state.draft)
-      );
-    };
-    reader.readAsDataURL(f[0]);
   }
 
   render() {
@@ -69,12 +95,15 @@ export default class ImagePane extends Component {
         <Form>
           <FormItem>
             <Label>Upload image</Label>
+            <div style={{position: 'absolute', bottom: 0, left: 0, overflow: 'hidden', pointerEvents: 'none'}}>
+              <img ref={img => {this.img = img;}} onLoad={() => this.onLoad()} alt="original" src="http://via.placeholder.com/350x150" />
+            </div>
             <Dropzone
               accept="image/jpeg, image/jpg, image/svg, image/gif, image/png"
               ref={(node) => {
                 this.dropzoneRef = node;
               }}
-              onDrop={(accepted, rejected) => {
+              onDrop={(accepted) => {
                 this.handleFile(accepted);
               }}
               style={{ display: "none" }}
