462d6d609b4f339fb4fb7e7d3d9b5e4ef3611f84
Alaa Batayneh <a.batayneh@gmail.com>
2018-03-27 15:17:33 +0300

    text changes

--- packages/composer/src/options/USER_ACTIONS.js
+++ packages/composer/src/options/USER_ACTIONS.js
@@ -1,39 +1,59 @@
 const USER_ACTIONS = {
   continue: {
     text: [
-      "Carry on",
-      "Details pls",
-      "Seriously?",
       "Tell me more",
-      "I’m intrigued",
-      "No way!",
-      "Fancy that",
-      "I wanna know more",
-      "Whaaat?"
+      "That’s interesting",
+      "Explain please",
+      "How do you know?",
+      "What else?",
+      "Really?",
+      "Says who?",
+      "Why?"
     ],
     image: [
-      "Pic or didn’t happen",
-      "Show me a picture",
-      "How does that look?",
-      "Got a photo?",
-      "Picture please",
-      "Pic pls"
+      "Photo please!",
+      "Says who?",
+      "Who’s he?",
+      "Who is she?",
+      "Was this documented?",
+      "Is there evidence?",
+      "Give me an image",
+      "Let me see!",
+      "Show me a photo"
     ],
     link: [
-      "Can you source this?",
-      "Show me a link",
-      "Where can I learn more?",
-      "Is it published somewhere?",
-      "Where can I find it?",
-      "Link please"
+      "Is this true?",
+      "More info please",
+      "How do you know?",
+      "Is this confirmed?",
+      "What’s the background?",
+      "Give me context!",
+      "What do experts say?",
+      "What’s the source?",
+      "Prove it!",
+      "Any documentation?"
     ],
     embed: [
       "Can you show this on a map?",
       "Is it on Youtube?",
       "Is it on Google Maps?"
     ],
-    map: ["Can you show this on a map?"],
-    media: ["Is it on Youtube?"]
+    map: [
+      "Where was this?",
+      "Where is that?",
+      "What’s the location?",
+      "Show me a map!",
+      "Don’t know that place"
+    ],
+    media: [
+      "Was this reported?",
+      "Did anyone film?",
+      "What’s the evidence?",
+      "What did the news say?",
+      "Is there footage?",
+      "Any witnesses?",
+      "Is it on youtube?"
+    ]
   },
   explore: {
     text: [
