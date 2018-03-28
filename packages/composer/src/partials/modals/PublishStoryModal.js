f8754366d6dd8e335d614873a3e2f2d2543d6036
Laurian Gridinoc <laurian@gridinoc.name>
2018-03-28 12:14:45 +0800

    testing lambda call

--- packages/composer/src/partials/modals/PublishStoryModal.js
+++ packages/composer/src/partials/modals/PublishStoryModal.js
@@ -3,7 +3,7 @@ import css from "styled-components";
 import React, { Component } from "react";
 import ReactModal from "react-modal";
 import { arrayOf, bool, func, number, object } from "prop-types";
-import { Storage } from "aws-amplify";
+import { API, Storage } from "aws-amplify";
 import shortUuid from "short-uuid";
 
 import {
@@ -105,8 +105,8 @@ export default class PublishStoryModal extends Component {
     .then (async result => {
       console.log(result);
 
-      // const { result2 } = await API.get("Story Operations", `/publish/${this.props.story.id}`, { response: true });
-      // console.log(result2);
+      const { result2 } = await API.get("Story", `/publish/${this.props.story.id}`, { response: true });
+      console.log(result2);
 
       this.setState({
         step: this.state.step + 1,
