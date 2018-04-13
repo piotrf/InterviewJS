79fc2e77d2e82ba26760e9db630a6779bfd870cc
Piotr F <piotr@piotrf.pl>
2018-04-13 22:04:33 +0200

    Mask Viewer cover only if image present.

--- packages/viewer/src/partials/Cover.js
+++ packages/viewer/src/partials/Cover.js
@@ -1,5 +1,5 @@
 import React from "react";
-import css from "styled-components";
+import styled from "styled-components";
 import { array, bool, oneOfType, node, string } from "prop-types";
 
 import {
@@ -10,7 +10,7 @@ import {
   setSpace
 } from "interviewjs-styleguide";
 
-const CoverEl = css(Container)`
+const CoverEl = styled(Container)`
   background-color: ${color.black};
   background-image: url(${({ image }) => image});
   background-position: center center;
@@ -21,10 +21,9 @@ const CoverEl = css(Container)`
   width: 100%;
 `;
 
-const CoverBody = css.div`
+const CoverBody = styled.div`
   ${setSpace("phm")};
   ${setSpace("ptm")};
-  background-color: ${color.shadowLt};
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -33,30 +32,37 @@ const CoverBody = css.div`
   ${breakpoint.tablet} {
     min-height: ${({ compact }) => (compact ? `${100 / 4}vh` : `${100 / 2}vh`)};
   }
-  &:after {
-    height: 50%;
-    background: linear-gradient(rgba(0,0,0,0), ${color.black});
-    bottom: 0;
-    content: " ";
-    display: block;
-    left: 0;
-    position: absolute;
-    right: 0;
-    z-index: 1;
-  }
+  ${({ hasImage }) =>
+    hasImage
+      ? `
+      background-color: ${color.shadowLt};
+      &:after {
+        height: 50%;
+        background: linear-gradient(rgba(0,0,0,0), ${color.black});
+        bottom: 0;
+        content: " ";
+        display: block;
+        left: 0;
+        position: absolute;
+        right: 0;
+        z-index: 1;
+      }
+
+  `
+      : ``};
 `;
 
-const CoverSauce = css.div`
+const CoverSauce = styled.div`
   ${setSpace("mbm")};
   position: relative;
   z-index: 2;
 `;
 
-const Brandmark = css.div`
+const Brandmark = styled.div`
   ${setSpace("mts")};
   ${setSpace("mbm")};
   line-height: 0;
-  opacity: .8;
+  opacity: 0.8;
   img {
     height: 36px;
   }
@@ -71,7 +77,7 @@ const Brandmark = css.div`
 
 const Cover = (props) => (
   <CoverEl {...props}>
-    <CoverBody compact={props.compact}>
+    <CoverBody compact={props.compact} hasImage={props.image}>
       <Brandmark>
         <img src={LogoWSymbolNegative} alt="InterviewJS" />
       </Brandmark>
