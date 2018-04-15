c3280e51b41eeadd3e938990eca440308d33dcc5
Piotr F <piotr@piotrf.pl>
2018-04-15 14:19:04 +0200

    Readonly story link in the Publish modal.

--- packages/composer/src/partials/modals/PublishStoryModal.js
+++ packages/composer/src/partials/modals/PublishStoryModal.js
@@ -245,9 +245,9 @@ export default class PublishStoryModal extends Component {
             <Separator size="s" silent />
             <TextInput
               input
-              disabled
-              value={`${iframeViewer}/`}
+              readonly="readonly"
               style={{ textAlign: "center" }}
+              value={`${iframeViewer}/`}
             />
             <Separator size="m" silent />
             <Actionbar>
